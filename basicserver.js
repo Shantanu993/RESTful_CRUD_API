d71968c8850e184a5edc316b40a9098467002239
Shantanu993 <shantanugahtori4@gmail.com>
2023-10-05 01:41:36 +0530

    connected mongodb

--- basicserver.js
+++ basicserver.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', (req, res) => {
@@ -12,3 +13,9 @@ app.get('/about', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
+
+mongoose.connect('mongodb+srv://Shantanu993:12345678Admin@cluster0.rdogfmh.mongodb.net/Node-API?retryWrites=true&w=majority')
+  .then(() => console.log('DB Connected!'))
+  .catch((err) => {
+    console.log("Error");
+  })
