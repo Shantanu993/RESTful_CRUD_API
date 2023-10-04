af8eaa547fef2f5db696cfe40a3dcda19b877de3
Shantanu993 <shantanugahtori4@gmail.com>
2023-10-05 02:38:21 +0530

    created collection

--- basicserver.js
+++ basicserver.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Products = require('./models/productModels')
+
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -10,6 +13,15 @@ app.get('/about', (req, res) => {
     res.send('Hello this is my about page!')
 })
 
+app.post('/product', async(req, res) => {
+  try {
+    const product = await Products.create(req.body)
+    res.status(200).json(product);
+    
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 
 mongoose.connect('mongodb+srv://Shantanu993:12345678Admin@cluster0.rdogfmh.mongodb.net/Node-API?retryWrites=true&w=majority')
--- models/productModels.js
+++ models/productModels.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const productSchema = new mongoose.Schema(
+const productSchema = mongoose.Schema(
     {
         name: {
             type: String,
@@ -39,7 +39,7 @@ const productSchema = new mongoose.Schema(
     },
     {
         timestamps: true //important
-    },
-    {
-
     })
+
+const Products = mongoose.model("Products", productSchema);
+module.exports = Products;
