3bce06e559f26a9af8bf23ac6f8731c974700f44
Shantanu993 <shantanugahtori4@gmail.com>
2023-10-06 21:55:34 +0530

    added dotenv file

--- basicserver.js
+++ basicserver.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Products = require('./models/productModels')
+const [MONGO_URL] = require('dotenv').config()
 
 const app = express()
 app.use(express.json())
+
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log('DB Connected!')
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000')
+    })
+  })
+  .catch((err) => {
+    console.error("Error in starting server:", err);
+  });
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -23,14 +35,3 @@ app.post('/product', async(req, res) => {
   }
 })
 
-
-mongoose.connect('mongodb+srv://Shantanu993:12345678Admin@cluster0.rdogfmh.mongodb.net/Node-API?retryWrites=true&w=majority')
-  .then(() => {
-    console.log('DB Connected!')
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000')
-    })
-  })
-  .catch((err) => {
-    console.log("Error");
-  })
